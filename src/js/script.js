71fac96b023ed86b7113d47c0f4b428139a39b64
Eduardo Peruzzo <eduardo.peruzzo05@gmail.com>
2024-12-09 14:07:13 -0300

    página do projeto

--- src/js/script.js
+++ src/js/script.js
@@ -24,36 +24,101 @@ function criaProjeto(projeto) {
     p.innerHTML = projeto.desc
 
     containerSecundario.appendChild(img)
-    containerPrincipal.appendChild(p)
 
     containerPrincipal.appendChild(h3)
     containerPrincipal.appendChild(containerSecundario)
+    containerPrincipal.appendChild(p)
+
+    containerSecundario.dataset.id = projeto.id
+    containerSecundario.onclick = manipulaClick
 
     containerProjetos.appendChild(containerPrincipal)
 }
 
-function arrumaPosicao() {
-    const descs = document.querySelectorAll(".desc")
+function criaDetalhes(id) {
+    let projetoFiltrado
+    projetoFiltrado = projetos.filter((projeto) => projeto.id == id)
+    projetoFiltrado = projetoFiltrado[0]
 
-    descs.forEach((p) => {
-        const elementoPai = p.parentElement;
+    const paginaProjeto = document.createElement("div")
+    paginaProjeto.classList.add("pagina-projeto")
+
+    const h1 = document.createElement("h1")
+
+    const img = document.createElement("img")
+
+    const p = document.createElement("p")
+
+    const fechar = document.createElement("button")
+
+    h1.innerHTML = projetoFiltrado.nome
+    img.src = projetoFiltrado.imagem
+    p.innerHTML = projetoFiltrado.desc
+    fechar.innerHTML = "x"
+    fechar.onclick = () => {
+        paginaProjeto.style.opacity = 0
+        setTimeout(() => {
+            document.body.removeChild(paginaProjeto)
+            document.body.classList.remove("sem-scroll")
+        }, 500);
+
+        const blurOverlay = document.querySelector(".blur-overlay");
+        blurOverlay.style.pointerEvents = "none";
+        blurOverlay.style.opacity = "0";
+    }
+
+    paginaProjeto.appendChild(h1)
+    paginaProjeto.appendChild(img)
+    paginaProjeto.appendChild(p)
+    paginaProjeto.appendChild(fechar)
+
+    document.body.appendChild(paginaProjeto)
+
+    setTimeout(() => {
+        paginaProjeto.classList.add("animacao")
+    }, 10);
+
+    const alturaPai = window.innerHeight;
+    const alturaElemento = paginaProjeto.offsetHeight;
+
+    const deslocamento = (alturaPai - alturaElemento) / 2;
+
+    paginaProjeto.style.top = `${deslocamento}px`;
+}
+
+function manipulaClick(evento) {
+    const id = evento.currentTarget.dataset.id
+
+    criaDetalhes(id)
+    arrumaPosicao("pagina-projeto")
+    // Bloqueia o scroll no fundo
+    document.body.classList.add("sem-scroll")
+
+    // Ativa o blur
+    const blurOverlay = document.querySelector(".blur-overlay");
+    blurOverlay.style.pointerEvents = "all";
+    blurOverlay.style.opacity = "1";
+}
+
+function arrumaPosicao(classe) {
+    const elementos = document.querySelectorAll(`.${classe}`)
+
+    elementos.forEach((elemento) => {
+        const elementoPai = elemento.parentElement;
 
         const larguraPai = elementoPai.offsetWidth;
-        const larguraElemento = p.offsetWidth;
+        const larguraElemento = elemento.offsetWidth;
 
-        // Cálculo para centralizar horizontalmente
         const deslocamento = (larguraPai - larguraElemento) / 2;
 
-        // Aplicar no estilo
-        p.style.left = `${deslocamento}px`;
-
+        elemento.style.left = `${deslocamento}px`;
     })
 }
 
 fetch("projetos.json").then((resposta) => {
     resposta.json().then((dados) => {
         dados.forEach(projeto => criaProjeto(projeto));
-
-        arrumaPosicao()
+        projetos = dados
+        arrumaPosicao("desc")
     })
 });
